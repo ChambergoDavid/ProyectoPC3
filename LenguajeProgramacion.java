8206e3f697fd714f0b604850fd32e5ea0ee66150
ChambergoDavid <mars.david.fr@gmail.com>
2024-11-24 22:44:51 -0500

    LenguajeProgramacion.java

---
+++ LenguajeProgramacion.java
@@ -0,0 +1,55 @@
+package puntospc3;
+
+public class LenguajeProgramacion {
+    private int anioCreacion;
+    private String caracteristicaPrincipal;
+    private String nombre;
+    private String utilizacion;
+
+    public LenguajeProgramacion(int anioCreacion, String caracteristicaPrincipal, String nombre, String utilizacion) {
+        this.anioCreacion = anioCreacion;
+        this.caracteristicaPrincipal = caracteristicaPrincipal;
+        this.nombre = nombre;
+        this.utilizacion = utilizacion;
+    }
+
+    public int getAnioCreacion() {
+        return anioCreacion;
+    }
+
+    public void setAnioCreacion(int anioCreacion) {
+        this.anioCreacion = anioCreacion;
+    }
+
+    public String getCaracteristicaPrincipal() {
+        return caracteristicaPrincipal;
+    }
+
+    public void setCaracteristicaPrincipal(String caracteristicaPrincipal) {
+        this.caracteristicaPrincipal = caracteristicaPrincipal;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUtilizacion() {
+        return utilizacion;
+    }
+
+    public void setUtilizacion(String utilizacion) {
+        this.utilizacion = utilizacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Lenguaje: " + nombre +
+                "\nAño de Creación: " + anioCreacion +
+                "\nCaracterística Principal: " + caracteristicaPrincipal +
+                "\nUtilización: " + utilizacion;
+    }
+}
